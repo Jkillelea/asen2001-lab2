5b70bd8b1e12bc0eee8db3fa64883af620a1666b
Jacob Killelea <Jkillelea@protonmail.ch>
2016-10-14 16:13:51 -0600

    deleted a bunch

--- src/magnitude.m
+++
@@ -1,8 +0,0 @@
-% Copyright Jacob Killelea, ASEN 2001 Lab 2, Fall 2016
-function m = magnitude(vector)
-	% calculates the magnitude of the 3D vector [a, b, c] according to to the formula sqrt(a^2 + b^2 + c^2)
-	a = vector(1).^2; % square them all
-	b = vector(2).^2;
-	c = vector(3).^2;
-	m = sqrt(a + b + c);
-end
--- src/unit_vector_from_points.m
+++
@@ -1,9 +0,0 @@
-function u_vector = unit_vector_from_points(vector_a, to_or_from, vector_b)
-  if to_or_from == 'to'
-    u_vector = to_unit_vector(vector_b - vector_a);
-  elseif to_or_from == 'from'
-    u_vector = to_unit_vector(vector_a - vector_b);
-  else
-    error('Must specify direction as either "to" or "from"!');
-  end
-end
